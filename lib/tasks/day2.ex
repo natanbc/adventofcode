e6cc563fa2853bce777325a5f7793552c79102ff
natanbc <natanbc@users.noreply.github.com>
2018-12-02 13:03:40 -0200

    more performance hacks
    Day 2 part 1: <snip> (4.247 ms)
    Day 2 part 2: <snip> (10.25 ms)

--- lib/tasks/day2.ex
+++ lib/tasks/day2.ex
@@ -4,38 +4,40 @@ defmodule Tasks.Day2 do
 
   def part1(input) do
     lines = String.split(input, ~r(\r?\n))
-    Enum.count(lines, fn line -> count_equal(String.to_charlist(line), 2) end) *
-      Enum.count(lines, fn line -> count_equal(String.to_charlist(line), 3) end)
+    map = Enum.reduce(lines, %{two: 0, three: 0}, fn line, %{two: two, three: three} ->
+      %{two: two + if count_equal(line, 2) do 1 else 0 end,
+        three: three + if count_equal(line, 3) do 1 else 0 end}
+    end)
+    map.two * map.three
   end
 
   def part2(input) do
     lines = String.split(input, ~r(\r?\n))
-    ids = Enum.flat_map(lines, fn line ->
-      Enum.filter(lines, fn line2 ->
+    [a, _] = pair = Enum.reduce_while(lines, nil, fn line,_ ->
+      res = Enum.filter(lines, fn line2 ->
         # This works because we only need to check if there's one different byte between the strings.
         # So, we can check if the length is equal to the common prefix length + the common suffix length + 1
         byte_size(line) -
           (:binary.longest_common_prefix([line, line2]) + :binary.longest_common_suffix([line, line2])) == 1
       end)
-    end) |> Enum.map(&String.to_charlist/1)
-    without_differing_char(hd(ids), hd(tl ids), [])
-    |> List.to_string()
+      if length(res) == 0 do
+        {:cont, nil}
+      else
+        {:halt, [line | res]}
+      end
+    end)
+    # Same hack as above
+    binary_part(a, 0, :binary.longest_common_prefix(pair)) <>
+    binary_part(a, byte_size(a), -:binary.longest_common_suffix(pair))
   end
 
   defp count_equal(line, count, map \\ %{})
-  defp count_equal([], count, map) do
-    Enum.reduce(map, false, fn {_, v}, acc -> acc || v == count end)
-  end
-  defp count_equal([ch | tail], count, map) do
-    count_equal(tail, count, Map.update(map, ch, 1, & &1 + 1))
-  end
-
-  defp without_differing_char([], [], acc), do: acc
-  defp without_differing_char([a | tailA], [b | tailB], acc) do
-    if a == b do
-      without_differing_char(tailA, tailB, acc ++ [a])
-    else
-      without_differing_char(tailA, tailB, acc)
+  defp count_equal(<<>>, _, _), do: false
+  defp count_equal(<<ch, tail::binary>>, count, map) do
+    case map do
+      %{^ch => c} when c == count - 1 -> true
+      %{^ch => c} -> count_equal(tail, count, %{map | ch => c + 1})
+      _ -> count_equal(tail, count, Map.put(map, ch, 1))
     end
   end
 end
